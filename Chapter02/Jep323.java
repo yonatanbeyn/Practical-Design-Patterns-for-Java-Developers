c9006ae28fc5401bb932e73be5dfa93a25d5d5db
yonatan beyn <Natan09@yahoo.com>
2023-08-27 00:20:29 -0500

    Update Jep323.java
    comment repeated

--- Chapter02/Jep323.java
+++ Chapter02/Jep323.java
@@ -5,6 +5,10 @@ import java.util.stream.IntStream;
  * JEP-323: Local-Variable Syntax for Lambda Parameters
  * {@link https://openjdk.java.net/jeps/323}
  */
+/**
+ * JEP-323: Local-Variable Syntax for Lambda Parameters
+ * {@link https://openjdk.java.net/jeps/323}
+ */
 public class Jep323 {
     public static void main(String[] args) {
         Consumer<Integer> consumer = (var number) -> {
